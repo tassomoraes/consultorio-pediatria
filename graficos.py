81f2edfab8e5ca85a450300edda81e2954ee2dcb
Tasso Moraes <tassoluis@gmail.com>
2021-09-30 05:03:16 -0300

    Gráfico curva crescimento atuzalizado

--- graficos.py
+++ graficos.py
@@ -1,10 +1,16 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+# mostra o gráfico da curva de crescimento comparando com
+# as curvas fornecidas pela OMS
 def curva_de_crescimento_peso(dados_y, nome_paciente):
+    #pegando apenas o primeiro nome
+    nome_paciente = nome_paciente.split()[0]
 
+    # a quantidade de meses passadas referente ao peso atual
     x_curva =           [0  ,2  ,4  ,12  ,24  ,36  ,60  ]
 
+    # referência de acordo com os meses de vida
     y_curva = np.array([[2  ,4  ,5  ,7   ,8.7 ,10  ,12.4],
                         [2.5,4.4,5.5,7.8 ,9.8 ,11.3,14  ],
                         [3.5,5.6,7  ,9.7 ,12.2,14.4,18.3],
